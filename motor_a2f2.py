33994c53a24543950a2dae844f2d1a85b58b92ae
RickF-dotcom <ricardoferreirasejusp@gmail.com>
2025-11-20 13:30:24 -0300

    Update motor_a2f2.py

--- motor_a2f2.py
+++ motor_a2f2.py
@@ -1,12 +1,11 @@
 # motor_a2f2.py
 # Wrapper oficial do motor A2F2
-# Este arquivo conecta o motor REAL ao nome padrão do ecossistema.
+# Este arquivo conecta o motor REAL (A2F2Engine) ao nome padrão usado pelo ecossistema.
 
 from a2f2_engine import A2F2Engine
 
-# Classe com o nome que o ecossistema A2F2 espera
 class MotorA2F2(A2F2Engine):
     pass
 
-# Alias adicional (alguns módulos procuram "Motor")
+# Alias extra para compatibilidade
 Motor = MotorA2F2
