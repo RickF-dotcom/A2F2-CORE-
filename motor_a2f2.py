2be042acb5702895afef1cf9a0d13c5d7f2fa8d4
RickF-dotcom <ricardoferreirasejusp@gmail.com>
2025-11-20 13:31:40 -0300

    Update motor_a2f2.py

--- motor_a2f2.py
+++ motor_a2f2.py
@@ -1,6 +1,6 @@
 # motor_a2f2.py
 # Wrapper oficial do motor A2F2
-# Este arquivo conecta o motor REAL (A2F2Engine) ao nome padrão usado pelo ecossistema.
+# Este arquivo conecta o motor REAL ao nome padrão usado pelo ecossistema.
 
 from a2f2_engine import A2F2Engine
 
