dcdf0d455028caea62f33972fd5e17d36f71d559
RickF-dotcom <ricardoferreirasejusp@gmail.com>
2025-11-20 13:37:23 -0300

    Update motor_a2f2.py

--- motor_a2f2.py
+++ motor_a2f2.py
@@ -1,11 +1,10 @@
 # motor_a2f2.py
 # Wrapper oficial do motor A2F2
-# Este arquivo conecta o motor REAL ao ecossistema.
 
 from a2f2_engine import A2F2Engine
 
 class MotorA2F2(A2F2Engine):
     pass
 
-# Alias extra para compatibilidade
+# Alias extra para compatibilidade interna
 Motor = MotorA2F2
